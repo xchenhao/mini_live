7c30f9e585d0b5b00a53bb342761c0d6d5752909
xchenhao <chenhao_apple@163.com>
2020-09-04 19:12:45 +0800

    fix cookie redirect

--- index.php
+++ index.php
@@ -12,12 +12,15 @@ foreach ($_POST as $key => $value) {
     sql_check($value);
 }
 foreach ($_COOKIE as $key => $value) {
-    sql_check($value);
+    sql_check($value, $key, $value);
 }
-function sql_check($sql_str) {
+function sql_check($sql_str, $cookie_key = null, $cookie_value = null) {
     // 自动过滤 SQL 的注入语句
     $check = preg_match('/select|EXEC|convert|declare|char|truncate|database|and|exec|sleep|cmd|where|set|alter|table|script|\'|\\*|\*|\.\.\/|\.\/|union|into|load_file|outfile/i', $sql_str);
     if ($check) {
+        if ($cookie_key) {
+            setcookie($cookie_key, 'deleted', $_SERVER['REQUEST_TIME']);
+        }
         header('Location: /');
         exit();
     }
